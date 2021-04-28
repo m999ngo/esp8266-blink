64f52d9256cae73dfdc65b88c0135326af330105
m999ngo <m999ngo@gmail.com>
2021-04-28 11:01:00 +0200

    par interruption

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
-const int LED = LED_BUILTIN;
-const unsigned long DELAY = 1000;
+const uint LED = LED_BUILTIN;
 
-void setup() {
-  pinMode(LED,OUTPUT);
-  Serial.begin(9600);
+// Le code de la fonction est mis en RAM au lieu d'être en Flash
+// important car une interruption peut-être utilisée n'importe quand
+// la fonction est invoquée sans accès à la mémoire Flash
+void ICACHE_RAM_ATTR toogleLED(void)
+{
+  // TODO : la routine d'interruption doit faire clignoter la LED
+  digitalWrite(LED,!digitalRead(LED));
+}
+
+
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  
+  timer1_attachInterrupt(toogleLED);
+  // horloge à 80 MHz
+  // diviseur par 16 => la fréquence est de 5 Mz (période de 0.2 µs)
+  timer1_enable(TIM_DIV16,TIM_EDGE,TIM_LOOP);
+  // comptage de 1 000 000 µs / 0.2 µs pour 1 seconde (soit la fréquence dans ce cas)
+  timer1_write(5000000);
 }
 
 void loop() {
-  static unsigned long now = millis();
-
-  if(millis()-now > DELAY){
-    now = millis();
-    digitalWrite(LED,!digitalRead(LED));
-    Serial.printf("STATE : %d\n",digitalRead(LED));
-    }
+  // put your main code here, to run repeatedly:
 }
